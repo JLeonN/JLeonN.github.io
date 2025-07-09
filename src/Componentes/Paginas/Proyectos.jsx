0f2008238af74379a3c0c7efad1cc0e060b80aa6
Leonardo Núñez <jorgebike.75@hotmail.com>
2025-07-09 07:07:09 -0300

    Click en la tarjeta
    Se añadió la función `manejarClick` para abrir el enlace del proyecto al hacer clic en la tarjeta. Prioriza el enlace de producción y, si no está presente, abre el de GitHub.

--- src/Componentes/Paginas/Proyectos.jsx
+++ src/Componentes/Paginas/Proyectos.jsx
@@ -1,22 +1,34 @@
 function Proyectos({ src, alt, titulo, descripcion, gitHub, enProduccion }) {
+  const enlaceProduccion = enProduccion?.props?.href;
+  const enlaceGitHub = gitHub?.props?.href;
+
+  const manejarClick = () => {
+    const enlaceDestino = enlaceProduccion || enlaceGitHub;
+    if (enlaceDestino) {
+      window.open(enlaceDestino, "_blank");
+    }
+  };
+
   return (
-    <>
-      <div className="tamañoTarjeta">
-        <div className="tarjeta">
-          <div className="tarjetaImagen">
-            <img src={src} alt={alt} />
-          </div>
-          <h3 className="tarjetaTitulo">{titulo}</h3>
-          <div className="descripcion">
-            <p>{descripcion}</p>
-          </div>
-          <div className="tarjetaIconos">
-            {gitHub}
-            {enProduccion}
-          </div>
+    <div
+      className="tamañoTarjeta"
+      onClick={manejarClick}
+      style={{ cursor: "pointer" }}
+    >
+      <div className="tarjeta">
+        <div className="tarjetaImagen">
+          <img src={src} alt={alt} />
+        </div>
+        <h3 className="tarjetaTitulo">{titulo}</h3>
+        <div className="descripcion">
+          <p>{descripcion}</p>
+        </div>
+        <div className="tarjetaIconos">
+          {gitHub}
+          {enProduccion}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
