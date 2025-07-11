ce1970018474700978432b8895fe072606f5b7ba
Leonardo Núñez <jorgebike.75@hotmail.com>
2025-07-11 19:47:07 -0300

    Agregar title a los iconos
    Se añadió el atributo title a cada imagen de los iconos de lenguajes en la sección de conocimientos para que, al pasar el cursor sobre ellos, se muestre el nombre correspondiente como tooltip. Esto mejora la accesibilidad y la experiencia de usuario al identificar fácilmente cada tecnología representada.

--- src/Componentes/Paginas/Conocimientos.jsx
+++ src/Componentes/Paginas/Conocimientos.jsx
@@ -8,41 +8,49 @@ const Conocimientos = () => {
             <img
               src="/Imagenes/IconosDeLenguajes/React.png"
               alt="React"
+              title="React"
               className="icono"
             />
             <img
               src="/Imagenes/IconosDeLenguajes/React-Native.png"
               alt="React Native"
+              title="React Native"
               className="icono"
             />
             <img
               src="/Imagenes/IconosDeLenguajes/React-Electron.png"
               alt="React Electron"
+              title="React Electron"
               className="icono"
             />
             <img
               src="/Imagenes/IconosDeLenguajes/Expo.png"
               alt="Expo"
+              title="Expo"
               className="icono"
             />
-              <img
-                src="/Imagenes/IconosDeLenguajes/JavaScript.png"
-                alt="JavaScript"
-                className="icono"
-              />
-              <img
-                src="/Imagenes/IconosDeLenguajes/CSS.png"
-                alt="CSS3"
-                className="icono"
-              />
+            <img
+              src="/Imagenes/IconosDeLenguajes/JavaScript.png"
+              alt="JavaScript"
+              title="JavaScript"
+              className="icono"
+            />
+            <img
+              src="/Imagenes/IconosDeLenguajes/CSS.png"
+              alt="CSS3"
+              title="CSS3"
+              className="icono"
+            />
             <img
               src="/Imagenes/IconosDeLenguajes/HTML.png"
               alt="HTML5"
+              title="HTML5"
               className="icono"
             />
             <img
               src="/Imagenes/IconosDeLenguajes/Bootstrap.png"
               alt="Bootstrap"
+              title="Bootstrap"
               className="icono"
             />
           </div>
